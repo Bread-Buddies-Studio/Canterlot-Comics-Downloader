976be02c64dc5741834f0ec3a104ec3ec09b0c27
ECX-0 <155266232+ECX-0@users.noreply.github.com>
2025-03-08 07:58:58 -0500

    Fixed Download Path and CBZ file creation
    Forgot people don't have the same download path as me... whoopsies.
    
    Fixed CBZ file creation, if the file already existed, CBZ file creation panicked, now it just replaces what was once there.

--- RequestForDownloadCanterlotComics/Program.cs
+++ RequestForDownloadCanterlotComics/Program.cs
@@ -14,6 +14,41 @@ using System.Drawing;
 using System.IO.Compression;
 using System.Windows.Media.Animation;
 using System.Reflection;
+using System.Runtime.InteropServices;
+public enum KnownFolder
+{
+    Contacts,
+    Downloads,
+    Favorites,
+    Links,
+    SavedGames,
+    SavedSearches
+}
+
+public static class KnownFolders
+{
+    private static readonly Dictionary<KnownFolder, Guid> _guids = new()
+    {
+        [KnownFolder.Contacts] = new("56784854-C6CB-462B-8169-88E350ACB882"),
+        [KnownFolder.Downloads] = new("374DE290-123F-4565-9164-39C4925E467B"),
+        [KnownFolder.Favorites] = new("1777F761-68AD-4D8A-87BD-30B759FA33DD"),
+        [KnownFolder.Links] = new("BFB9D5E0-C6A9-404C-B2B2-AE6DB6AF4968"),
+        [KnownFolder.SavedGames] = new("4C5C32FF-BB9D-43B0-B5B4-2D72E54EAAA4"),
+        [KnownFolder.SavedSearches] = new("7D1D3A04-DEBB-4115-95CF-2F29DA2920DA")
+    };
+
+    public static string GetPath(KnownFolder knownFolder)
+    {
+        return SHGetKnownFolderPath(_guids[knownFolder], 0);
+    }
+
+    [DllImport("shell32",
+        CharSet = CharSet.Unicode, ExactSpelling = true, PreserveSig = false)]
+    private static extern string SHGetKnownFolderPath(
+        [MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags,
+        nint hToken = 0);
+}
+
 public static class ZipFileCreator
 {
     /// <summary>
@@ -39,9 +74,9 @@ public static class ZipFileCreator
 internal static class Program
 {
     // Constants //
-    const string baseURL = "https://www.canterlotcomics.com";
+    const string baseURL = "https://www.canterlotcomics.com"; // Canterlot Comics Website //
     // Settings //
-    const string downloadLocation = @"C:\Users\Bread\Downloads";
+    static readonly string downloadLocation = KnownFolders.GetPath(KnownFolder.Downloads);
     static string comicLink = "https://www.canterlotcomics.com/comic/en/alien_twilight_signing_off-1959";
     // Don't Touch //
     static string comicName = comicLink[(comicLink.LastIndexOf('/') + 1)..(comicLink.LastIndexOf('-'))];
