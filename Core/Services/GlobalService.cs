15fc6c21547504dc0c55c2cd9217d551457d791d
ECX-0 <155266232+ECX-0@users.noreply.github.com>
2025-11-19 15:58:29 -0500

    Somehow this didn't go with the rest of the commit

--- Core/Services/GlobalService.cs
+++ Core/Services/GlobalService.cs
@@ -14,9 +14,11 @@ public static class GlobalService
     public const string baseURL = "https://www.canterlotcomics.com"; // Canterlot Comics Website //
     public const string ProgramName = "CanterlotDownloader";
     public static readonly string ProgramFiles = @$"{Environment.ExpandEnvironmentVariables("%ProgramW6432%")}\{ProgramName}";
+    public static readonly string LocalAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    public static readonly string AppData = $@"{LocalAppData}\{ProgramName}";
 
-    public static readonly string PanelsTemp = $@"{ProgramFiles}\PanelsTemp";
-    public static readonly string UpscaledTemp = $@"{ProgramFiles}\UpscaledTemp";
+    public static readonly string PanelsTemp = $@"{AppData}\PanelsTemp";
+    public static readonly string UpscaledTemp = $@"{AppData}\UpscaledTemp";
     public static readonly string UpscalerDirectory = $@"{ProgramFiles}\Upscaler";
     public static readonly string UpscalerEXE = $@"{UpscalerDirectory}\Upscaler.exe";
 }
